a5aac4341a01c505ee327e11dab19f9a53245746
AbogeJr <junioraboge@gmail.com>
2023-08-17 00:24:20 +0300

    update: added files

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import redis
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        self.key = uuid.uuid4()
+        self._redis.store(key, data)
+        return key
